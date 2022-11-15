34b6bafc381ddc515e44f1252e3c8e4d2c36864d
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-11-15 17:55:53 -0600

    create NoteForm, install react select

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { NewNote } from './components/NewNote'
 
 function App() {
   return (
     <Container className='my-4'>
       <Routes>
         <Route path='/' element={} />
-        <Route path='/new' element={} />
+        <Route path='/new' element={<NewNote />} />
         <Route path='/:id'>
           <Route index element={} />
           <Route path='/edit' element={} />
