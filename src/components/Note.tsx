9b469a7be8306a3eda97d8e6515acb513755cb84
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-11-15 20:18:04 -0600

    add onDelete to Note

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,10 +1,15 @@
 import { Row, Col, Stack, Badge, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useNote } from "./NoteLayout";
 import ReactMarkdown from 'react-markdown'
 
-export function Note() {
+type NoteProps = {
+    onDelete: (id: string) => void
+}
+
+export function Note({ onDelete }: NoteProps) {
     const note = useNote()
+    const navigate = useNavigate()
 
     return (<>
         <Row className='align-items-center mb-4'>
@@ -27,7 +32,12 @@ export function Note() {
                     <Link to={`/${note.id}/edit`}>
                         <Button variant='primary'>Edit</Button>
                     </Link>
-                    <Button variant='outline-danger'>Delete</Button>
+                    <Button
+                        variant='outline-danger'
+                        onClick={() => {
+                            onDelete(note.id)
+                            navigate('/')
+                        }}>Delete</Button>
                     <Link to='/'>
                         <Button variant='outline-secondary'>Back</Button>
                     </Link>
