c92ce230f0fcac8e4cd5a0d3604e3800d52ff508
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-11-15 18:33:44 -0600

    add create note function to App, NewNote, and Noteform

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,55 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useMemo } from 'react'
 import { Container } from 'react-bootstrap'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { NewNote } from './components/NewNote'
+import { useLocalStorage } from './utils/useLocalStorage'
+import { v4 as uuidV4 } from 'uuid'
+
+export type Note ={
+  id: string
+} & NoteData
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+export type RawNote = {
+  tagIds: any
+  id: string
+
+} & RawNoteData
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+export type Tag = {
+  id: string
+  label: string
+}
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', [])
+  const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prev => {
+      return [...prev, { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) }]
+    })
+  }
+
   return (
     <Container className='my-4'>
       <Routes>
         <Route path='/' element={} />
-        <Route path='/new' element={<NewNote />} />
+        <Route path='/new' element={<NewNote onSubmit={onCreateNote} />} />
         <Route path='/:id'>
           <Route index element={} />
           <Route path='/edit' element={} />
